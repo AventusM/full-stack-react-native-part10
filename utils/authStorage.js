7a1aee6244da92607b44e3cc5b214945ecf9a62c
AventusM <8antonm@gmail.com>
2020-11-19 22:37:08 +0200

    10.14 should be done

--- utils/authStorage.js
+++ utils/authStorage.js
@@ -5,16 +5,23 @@ class AuthStorage {
     this.namespace = namespace;
   }
 
-  getAccessToken() {
-    // Get the access token for the storage
+  // Get the access token for the storage
+  async getAccessToken() {
+    const token = await AsyncStorage.getItem(`${this.namespace}:token`);
+    return token ? JSON.parse(token) : [];
   }
 
-  setAccessToken(accessToken) {
-    // Add the access token to the storage
+  // Add the access token to the storage
+  async setAccessToken(accessToken) {
+    await AsyncStorage.setItem(
+      `${this.namespace}:token`,
+      JSON.stringify(accessToken)
+    );
   }
 
-  removeAccessToken() {
-    // Remove the access token from the storage
+  // Remove the access token from the storage
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:token`);
   }
 }
 

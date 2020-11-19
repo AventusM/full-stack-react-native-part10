d588e567bb71f1e3bf9544db83c6d7672b08d365
AventusM <8antonm@gmail.com>
2020-11-19 19:00:43 +0200

    10.12 done

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/react-hooks";
+//import Constants from "expo-constants";
 
 import Main from "./components/Main";
 import createApolloClient from "./utils/apolloClient";
@@ -8,6 +9,7 @@ import createApolloClient from "./utils/apolloClient";
 const apolloClient = createApolloClient();
 
 const App = () => {
+  //console.log(Constants.manifest);
   return (
     <NativeRouter>
       <ApolloProvider client={apolloClient}>
---
+++ app.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+import 'dotenv/config';
+
+export default {
+  name: "rate-repository-app",
+  slug: "rate-repository-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  extra: {
+    env: process.env.ENV,
+    apollo_uri: process.env.APOLLO_URI
+  },
+};

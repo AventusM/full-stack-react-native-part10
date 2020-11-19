ef0ccb5600f15a0ac4fdeb9a14468cbecab0babd
AventusM <8antonm@gmail.com>
2020-11-19 22:02:00 +0200

    10.13 done

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -7,6 +7,7 @@ import Text from "./Text";
 import FormikTextInput from "./FormikTextInput";
 
 import theme from "../theme";
+import useSignIn from "../hooks/useSignIn";
 
 const validationSchema = yup.object().shape({
   username: yup.string().required("Username is required"),
@@ -48,8 +49,17 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log("submitted values:", values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log("signIn success", data);
+    } catch (e) {
+      console.log("signIn error", e);
+    }
   };
 
   return (
